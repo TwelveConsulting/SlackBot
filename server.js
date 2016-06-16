07da9cc33eff8d7e5af556e1dc9a731879d292af
Gaspard Hosteins <Gaspard@MacBook-Air-de-Gaspard.local>
2016-06-16 13:00:29 +0200

    v0.154

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ bodyParser = require('body-parser'),
               "footer_icon": "https://pbs.twimg.com/profile_images/603464163701166080/SItfdpqV.jpg",
             }]
          };
-        convo.say(message,reply_with_attachments);
+        convo.say(reply_with_attachments);
       }
       bot.startConversation(message, askReserver);
     });
