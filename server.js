c3d812e6c79401b932c3875f7a5b5e8f952e9464
Gaspard Hosteins <Gaspard@MacBook-Air-de-Gaspard.local>
2016-06-16 15:52:10 +0200

    app.json

--- server.js
+++ server.js
@@ -79,6 +79,7 @@ bodyParser = require('body-parser'),
           convo.ask('Voulez vous réservez la salle de réunion de chez Twelve Consulting? (oui/non)', function(response, convo){
             if (response.text == 'non') {
               convo.say('OK désolé de vous avoir dérangé(e)');
+              convo.next();
             }
             else {
               askDate(response, convo);
