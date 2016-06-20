21303def9cdf086e4ce622d16849298e9b17504c
Gaspard Hosteins <Gaspard@MacBook-Air-de-Gaspard.local>
2016-06-20 17:54:56 +0200

    v0.24

--- server.js
+++ server.js
@@ -438,11 +438,11 @@ moment().format();
 -------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------*/
 
-  controller.hears(['date'], 'direct_message', (bot, message) => {
+  /*controller.hears(['date'], 'direct_message', (bot, message) => {
     var now = new Date();
     var res = { "date": now};
     bot.say(message,res.date);
-  })
+  })*/
 
 
 
