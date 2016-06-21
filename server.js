36785c1d6e09238896c7c5051bcbf2c2a7967103
Gaspard Hosteins <Gaspard@pc50.home>
2016-06-21 23:31:02 +0200

    v0.24.4

--- server.js
+++ server.js
@@ -437,8 +437,23 @@ bot.startRTM(err => {
 -------------------------------------------------------------------------------------------------------------*/
 
   controller.hears(['jour'], 'direct_message', (bot, message) => {
-    var now = moment().format('LLLL');
-    bot.reply(message,now);
+    askJour = function(response, convo){
+      var dateMois = /[0-9]?[0-9]) (janvier|fevrier|mars|avril|mai|juin|juillet|aout|septembre|octobre|novembre|decembre)?/;
+      var jourSeul = /dimanche|lundi|mardi|mercredi|jeudi|vendredi|samedi/;
+      convo.ask('Donne moi une date ?');
+      convo.next();
+      var tabDate = dateMois.exec(response.text);
+      var tabJour = jourSeul.exec(response.text);
+      /*if (tabD.length()=2) {
+        var m = moment()
+
+      }*/
+      var m=moment().year('2016').month(tabDate[1]).date(tabDate[0]);
+      convo.say(m);
+      convo.next;
+    }
+    
+    bot.startConversation(message, askJour);
   })
 
 
